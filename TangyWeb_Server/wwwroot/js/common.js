750394da7362b9ff2b104b5e058a1b8ce6151e20
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-03-07 22:31:44 +0700

    Toastr

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,9 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Успешная операция', 5000)
+    }
+    if (type === "error") {
+        toastr.error(message, 'Операция неудачна', 5000 )
+    }
+
+}
