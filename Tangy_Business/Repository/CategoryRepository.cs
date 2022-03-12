1951652763ca9a8cd127501672001a4f3a50c9a3
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-03-13 00:26:31 +0700

    Сделаны все crud для Categories.

--- Tangy_Business/Repository/CategoryRepository.cs
+++ Tangy_Business/Repository/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,46 +14,73 @@ namespace Tangy_Business.Repository
     public class CategoryRepository : ICategoryRepository
     {
         private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
 
-        public CategoryRepository(ApplicationDbContext db)
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
         {
             _db = db;
+            _mapper = mapper;
         }
         public CategoryDTO Create(CategoryDTO objDTO)
         {
-            Category category = new Category()
-            {
-                Name = objDTO.Name, 
-                Id = objDTO.Id,
-                CreatedDate = DateTime.Now
-            };
-            _db.Categories.Add(category);
+            //Category category = new Category()
+            //{
+            //    Name = objDTO.Name, 
+            //    Id = objDTO.Id,
+            //    CreatedDate = DateTime.Now
+            //};
+
+            var obj = _mapper.Map<CategoryDTO, Category>(objDTO);   
+            obj.CreatedDate = DateTime.Now;
+            var addedObj = _db.Categories.Add(obj);
             _db.SaveChanges();
-            return new CategoryDTO()
-            {
-                Id = category.Id,
-                Name = category.Name
-            };
+
+            return _mapper.Map<Category, CategoryDTO>(addedObj.Entity);
+
+            //return new CategoryDTO()
+            //{
+            //    Id = category.Id,
+            //    Name = category.Name
+            //};
         }
 
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj != null)
+            {
+                _db.Categories.Remove(obj);
+                return _db.SaveChanges();
+            }
+            return 0;
         }
 
         public CategoryDTO Get(int id)
         {
-            throw new NotImplementedException();
+            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj != null)
+            {
+                 return _mapper.Map<Category, CategoryDTO>(obj);
+            }
+            return new CategoryDTO();
         }
 
         public IEnumerable<CategoryDTO> GetAll()
         {
-            throw new NotImplementedException();
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
         }
 
         public CategoryDTO Update(CategoryDTO objDTO)
         {
-            throw new NotImplementedException();
+            var objDromDb = _db.Categories.FirstOrDefault(u => u.Id == objDTO.Id);
+            if(objDromDb!=null)
+            {
+                objDromDb.Name = objDTO.Name;
+                _db.Categories.Update(objDromDb);
+                _db.SaveChanges();
+                return _mapper.Map<Category, CategoryDTO>(objDromDb);
+            }
+            return objDTO;
         }
     }
 }
