3d54eeea20d2af66a8c05625b37a911e916e6d03
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-03-28 01:51:26 +0700

    Цены на продукт, CRUD для ProductPrice, Blazor Syncfusion DataGrid

---
+++ Tangy_Models/ProductPriceDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class ProductPriceDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Выберите продукт")]
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Выберите размер")]
+        public string Size { get; set; }
+        [Required(ErrorMessage = "Цена обязательна")]
+        [Range(1, int.MaxValue, ErrorMessage = "Цена должна быть больше 1")]
+        public double Price { get; set; }
+    }
+}
