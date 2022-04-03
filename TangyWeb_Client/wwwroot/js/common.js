d7ea1e44a8e3d51f0e0a2c842e17893cf5c3df60
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-04-04 03:10:15 +0700

    TangyWeb_Client - реализация вызовов API, получение/отображение списка продуктов, деталей продукта.

---
+++ TangyWeb_Client/wwwroot/js/common.js
@@ -0,0 +1,35 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Успешная операция', 5000)
+    }
+    if (type === "error") {
+        toastr.error(message, 'Операция неудачна', 5000 )
+    }
+}
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Уведомление об успехе!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Уведомление о НЕ успехе!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal()
+{
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
