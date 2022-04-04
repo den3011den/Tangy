5574d60ea3e4e58e27ec9b7fa147b2f04b323d5d
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-04-05 00:54:58 +0700

    Добавление, уменшение количества продукта и удаление продукта в списке отображения корзины покупок.

--- TangyWeb_Client/Service/CartService.cs
+++ TangyWeb_Client/Service/CartService.cs
@@ -9,6 +9,7 @@ namespace TangyWeb_Client.Service
     {
 
         private readonly ILocalStorageService _localStorage;
+        public event Action OnChange;
 
         public CartService(ILocalStorageService localStorage)
         {
@@ -20,50 +21,53 @@ namespace TangyWeb_Client.Service
             var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
             //if count is 0 or 1 then we will remove the item
 
-            for(int i=0; i<cart.Count; i++)
+            for (int i = 0; i < cart.Count; i++)
             {
                 if (cart[i].ProductId == cartToDecrement.ProductId && cart[i].ProductPriceId == cartToDecrement.ProductPriceId)
                 {
-                    if (cart[i].Count==1 || cart[i].Count == 0)
+
+                    if (cart[i].Count == 1 || cartToDecrement.Count == 0)
                     {
                         cart.Remove(cart[i]);
                     }
                     else
                     {
-                        cart[i].Count = -cartToDecrement.Count;
+                        cart[i].Count -= cartToDecrement.Count;
                     }
                 }
             }
 
             await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
+            OnChange.Invoke();
         }
 
         public async Task IncrementCart(ShoppingCart cartToAdd)
         {
             var cart = await _localStorage.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
             bool itemInCart = false;
-            if (cart==null)
+            if (cart == null)
             {
                 cart = new List<ShoppingCart>();
             }
-            foreach(var obj in cart)
+            foreach (var obj in cart)
             {
-                if(obj.ProductId==cartToAdd.ProductId && obj.ProductPriceId==cartToAdd.ProductPriceId)
+                if (obj.ProductId == cartToAdd.ProductId && obj.ProductPriceId == cartToAdd.ProductPriceId)
                 {
                     itemInCart = true;
                     obj.Count += cartToAdd.Count;
                 }
             }
-            if(!itemInCart)
+            if (!itemInCart)
             {
                 cart.Add(new ShoppingCart()
                 {
-                    ProductId = cartToAdd.ProductId,    
-                    ProductPriceId = cartToAdd.ProductPriceId, 
+                    ProductId = cartToAdd.ProductId,
+                    ProductPriceId = cartToAdd.ProductPriceId,
                     Count = cartToAdd.Count
                 });
             }
             await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
+            OnChange.Invoke();
         }
     }
 }
