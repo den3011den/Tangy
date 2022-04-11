13116bb095b5d5d8288d31cbf01a94cc2a952eef
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-04-12 01:08:27 +0700

    Добавление DTOs для работы с аутентификацией в Blazor WASM проекте.

---
+++ Tangy_Models/SignInRequestDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SignInRequestDTO
+    {
+        [Required(ErrorMessage = "Требуется имя")]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Невернре имя пользователя (должно быть как Email адрес)")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Требуется пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Tangy_Models/SignUpRequestDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SignUpRequestDTO
+    {
+        [Required(ErrorMessage = "Требуется имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Требуется Email")]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Неверный Email адрес")]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Требуется пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Требуется подтверждение пароля")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль и подтверждение пароля не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
