3ed6fd6418ca60c5832d4070c996860a9cd9b497
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2022-04-12 08:41:08 +0700

    Перевод кода регистрациия в codebehind

---
+++ TangyWeb_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+
+        private SignUpRequestDTO SignUpRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await _authService.RegisterUser(SignUpRequest);
+            if (result.IsRegistrationSuccessful)
+            {
+                // регистрация успешна
+                _navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+            }
+            IsProcessing = false;
+        }
+
+
+    }
+}
